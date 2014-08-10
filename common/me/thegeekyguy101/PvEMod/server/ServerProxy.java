b5c25f2cc92eaa1fe05abcc0ddd504d268ef0eea
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-10 22:00:35 +1000

    Getting ready for 1.7.10

--- common/me/thegeekyguy101/PvEMod/server/ServerProxy.java
+++
@@ -1,9 +0,0 @@
-package me.thegeekyguy101.PvEMod.server;
-
-public class ServerProxy {
-
-	public void registerRenderThings() {
-
-	}
-
-}
---
+++ src/main/java/net/rystuff/pvemod/PvEMod.java
@@ -0,0 +1,4 @@
+package net.rystuff.pvemod;
+
+public class PvEMod {
+}
